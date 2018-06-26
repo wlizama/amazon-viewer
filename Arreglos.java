77b52b2a291d456aee80699e61f35b8c2be311f8
Wilder Lizama <wilderlizama@gmail.com>
2018-06-25 22:50:44 -0500

    Arreglos en java

---
+++ Arreglos.java
@@ -0,0 +1,20 @@
+
+public class Arreglos {
+	
+	//Arrays
+	//Declaración de array
+	
+	int[] arregloInt = new int[2];
+	double arregloDouble[];
+	
+	int[][] array2D = new int[2][3];//caben 6 cupcakes
+	int[][][] array3D = new int[3][3][2]; //caben 18 cokies
+	//int[][][][] array4D = new int[1][2][3][4];
+	
+	char[][] days = { {'M','T','W'}, {'M', 'T', 'W'} };
+	
+	char[][][] letters = { { {'a','b'}, {'a','b'},{'a','b'} }, { {'a','b'}, {'a','b'}, {'a','b'} } };
+	
+
+
+}
